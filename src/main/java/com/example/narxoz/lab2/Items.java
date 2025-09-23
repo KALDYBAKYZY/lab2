e7f568ff48145d79cb09e40f99e03a2f7368881c
Gulnaz Zhaiylgan <gulnazzhaiylgan@MacBook-Pro-Gulnaz.local>
2025-09-23 11:56:41 +0500

    First commit

---
+++ src/main/java/com/example/narxoz/lab2/Items.java
@@ -0,0 +1,14 @@
+package com.example.narxoz.lab2;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Items {
+    Long id;
+    String name;
+    String description;
+    double price;
+}
